13d9487514cd58355fff30c33f253cb3cb408990
Andrés <andreslugo51@gmail.com>
2023-01-26 11:14:02 -0400

    primera clase

---
+++ main.js
@@ -0,0 +1,11 @@
+var miNumero = 5;
+var miAlfabetico = 'holis';
+var miBooleano = true;
+
+console.log(miNumero);
+console.log(miAlfabetico);
+console.log(miBooleano);
+
+console.log(typeof(miNumero));
+console.log(typeof(miAlfabetico));
+console.log(typeof(miBooleano));

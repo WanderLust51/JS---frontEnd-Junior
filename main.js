e0ba0bef982167d89bd4579c990e15d3baf3915b
andrés <andreslugo51@gmail.com>
2023-02-02 09:22:58 -0400

    Mitad de la segunda clase ♥

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-var miNumero = 5;
-var miAlfabetico = 'holis';
-var miBooleano = true;
+var superNum;
+var otherNum;
 
-console.log(miNumero);
-console.log(miAlfabetico);
-console.log(miBooleano);
+superNum = 15;
+otherNum = 8;
 
-console.log(typeof(miNumero));
-console.log(typeof(miAlfabetico));
-console.log(typeof(miBooleano));
+var bigNum = superNum + otherNum;
+
+var newNum = 5;
+newNum += bigNum;
+
+console.log('La variable newNum es la suma de newNum (5) y bigNum (23)')
+console.log(`Resultado de newNum = ${newNum}`)
+
+var randNum
+randNum = Math.random() * (superNum - otherNum) + otherNum
+console.log(`randNum es: ${randNum}`)
+console.log(`randNum con toFixed es: ${randNum.toFixed(2)}`)
+console.log(`randNum con toPrecision es: ${randNum.toPrecision(2)}}`)
+console.log(`randNum redondeado es: ${Math.ceil(randNum)}`)
+console.log(`randNum redondeado abajo es: ${Math.floor(randNum)}`)
+
+console.log(`El valor de los tickets es de: ${Math.round(Math.random() * 100)}`)

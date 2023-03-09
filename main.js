45c14399b6ee03498fc0a5ab7e25727ad0b73666
Andrés <andreslugo51@gmail.com>
2023-03-09 10:58:04 -0400

    tercera super clase de js2♥

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+superBtn = document.getElementById('superBtn')
+
+superBtn.addEventListener('click', presion)
+
+function presion(){
+    console.log('Me siento presionado')
+}
+
 function abrirVentanita(){
     alto = 0.5 * screen.availHeight;
     ancho = 0.5 * screen.availWidth;
@@ -11,4 +19,26 @@ function abrirVentanita(){
     window.resizeTo(tamañoAlto, tamañoAncho);
 }
 
-// abrirVentanita()
+function superFuncion(){
+    var nombre = prompt('Ingrese nombre');
+    elTitulo = document.getElementById('superTitulo');
+    elTitulo.innerHTML = nombre;
+}
+function superColor(){
+    var color = prompt('Super color:');
+    elTitulo = document.getElementById('superTitulo');
+    elTitulo.style.color = color;
+}
+
+function facturar(){
+    let superfactura = document.factura;
+    let facturaFinal = document.getElementById('facturaFinal');
+    
+    let nombre = superfactura.nombre.value;
+    let desc = superfactura.descripcion.value;
+    let precio = superfactura.precio.value;
+    let cantidad = superfactura.cantidad.value;
+
+    facturaFinal.innerHTML = `<p>${nombre}</p>`
+    console.log(facturaFinal)
+}
